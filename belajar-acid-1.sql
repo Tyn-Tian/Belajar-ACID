17f89656200b6dac76537eef58d0d5518d1d5fd6
lukxnut <kristian2015ok@gmail.com>
2024-04-18 21:18:36 +0700

    Add: Isolation

--- belajar-acid-1.sql
+++ belajar-acid-1.sql
@@ -34,3 +34,15 @@ where id = "Tian";
 commit;
 
 select * from accounts;
+
+start transaction;
+
+select * from accounts where id in ("Tian", "Budi") for update;
+
+update accounts set balance = balance - 20000
+where id = "Tian";
+
+update accounts set balance = balance + 20000
+where id = "Budi";
+
+commit;
--- belajar-acid-2.sql
+++ belajar-acid-2.sql
@@ -1,1 +1,13 @@
 select * from accounts;
+
+start transaction;
+
+select * from accounts where id in ("Tian", "Budi") for update;
+
+update accounts set balance = balance - 20000
+where id = "Tian";
+
+update accounts set balance = balance + 20000
+where id = "Budi";
+
+commit;
