d5d5a8afc4d1024a296f6eaaf696e54f6f653217
lukxnut <kristian2015ok@gmail.com>
2024-04-18 20:35:23 +0700

    Fix: Atomicity

--- belajar-acid-1.sql
+++ belajar-acid-1.sql
@@ -1,7 +1,27 @@
 create database belajar_acid;
 
-create table account (
+create table accounts (
 	id varchar(100) not null primary key,
     name varchar(100) not null,
     balance bigint not null
 ) engine = InnoDB;
+
+start transaction;
+
+insert into accounts(id, name, balance)
+values ("Tian", "Christian", 1250000);
+
+insert into accounts(id, name, balance)
+values ("Budi", "Budi Nugraha", 800000);
+
+commit;
+
+start transaction;
+
+delete from accounts where id = "Tian";
+
+delete from accounts where id = "Budi";
+
+select * from accounts;
+
+rollback;
